ca2cbe6f31edcf67f2161bb29e87cbec62fe7457
sharadjagtap <58584006+sharadjagtap@users.noreply.github.com>
2020-05-07 11:15:27 +0530

    Update settings.py

--- timeapp/timeapp/settings.py
+++ timeapp/timeapp/settings.py
@@ -76,14 +76,11 @@ WSGI_APPLICATION = 'timeapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mydb',
-        'USER': 'root',
-        'PASSWORD':'root123',
-        'HOST':'localhost',
-        'PORT':3306
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 
 # Password validation
